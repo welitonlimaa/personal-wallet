dbe94672360803caf35b32ddc7065681f352ee8e
Weliton.lima <welitonlima159@gmail.com>
2022-10-31 15:41:17 -0300

    iniciando trybewallet

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,13 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const changeUser = (data) => ({
+  type: LOGIN,
+  data,
+});
+
+export const DADOSWALLET = 'DADOS_WALLET';
+
+export const wallet = (data) => ({
+  type: DADOSWALLET,
+  data,
+});
